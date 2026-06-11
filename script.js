140b2fccee75b8f5fdb321e6f29af11b688fb3b7
rinsan1120 <keiz5963@gmail.com>
2026-06-11 13:39:46 +0900

    a

--- script.js
+++ script.js
@@ -106,6 +106,15 @@ const casts = [
     ]
   }
 ];
+const slides = document.querySelectorAll(".hero-slideshow .slide");
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}, 5000); // 5秒ごとに切替
+
 
 const archiveSelect = document.getElementById("archiveSelect");
 const archiveList = document.getElementById("archiveList");
